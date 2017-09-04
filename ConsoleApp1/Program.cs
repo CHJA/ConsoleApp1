8d92609cf230078221c16760f94268d45bfd16c7
CHJA <chenjijun222@hotmail.com>
2017-09-04 18:08:15 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ml = 0;
+            int sp = 0;
+            int len = 7000;
+            int tlen = 2000;
+            int step = 30;
+
+            //ml = ((len + tlen) + sp + tlen - step) % (len + tlen);
+            //sp = ml - tlen;
+
+            // Timer timer = new Timer(100);
+
+            ml = -2000;
+
+            while (true)
+            {
+                ml = ((len + tlen) + sp + tlen - step) % (len + tlen);
+                sp = ml - tlen;
+                Console.WriteLine(ml + "<----->" + sp);
+            }
+
+        }
+    }
+}
